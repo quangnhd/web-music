f5eb5efc4edbfe53510ce149ee5903b2deec13e6
quangnhd <quangnguyen10893@gmail.com>
2022-09-06 17:08:05 +0700

    fix link

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ let apiurl;
 if (NODE_ENV == 'development') {
   apiurl = 'http://localhost:3005/api/';
 } else {
-  apiurl = 'https://.herokuapp.com/api/';
+  apiurl = 'https://web-music-api.herokuapp.com/api/';
 }
 export const API_URL = apiurl;
